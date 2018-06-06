3d555a964e1de150787c72d6e00d2fa16f69c0f8
Your Name <you@example.com>
2018-06-07 05:58:04 +0600

    working mail script for timeweb mail

--- send.php
+++ send.php
@@ -1,21 +1,17 @@
 <?
-if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['theme'])&&$_POST['theme']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
-        $to = 'russs_sl@mail.ru'; //Почта получателя, через запятую можно указать сколько угодно адресов
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['theme'])&&$_POST['theme']!="")&&(isset($_POST['email'])&&$_POST['email']!="")&&(isset($_POST['message'])&&$_POST['message']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+        $to = 'info@fit-office.net'; //Почта получателя, через запятую можно указать сколько угодно адресов
         $subject = 'Заявка с сайта fit-office'; //Загаловок сообщения
-        $message = '
-                <html>
-                    <head>
-                        <title>'.$subject.'</title>
-                    </head>
-                    <body>
-                        <p>Имя: '.$_POST['name'].'</p>
-                        <p>Телефон: '.$_POST['phone'].'</p>                        
-                    </body>
-                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $from = $_POST['email'];
+        $message = ''.$subject.'
+                Имя: '.$_POST['name'].'
+                      Тема: '.$_POST['theme'].'
+                        Сообщение: '.$_POST['message'].'                    
+                        email: '.$_POST['email'].''; //Текст нащего сообщения можно использовать HTML теги
         $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
-        $headers = "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
+        $headers = 'From:' .$from.''; //Наименование и почта отправителя
         mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
-        echo $to;
+        echo $message;
 }
 ?>
 
