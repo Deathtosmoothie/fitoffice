eb31b224c3a2832c57f60c29a2fb5eb2d12089d9
Your Name <you@example.com>
2018-06-15 06:58:15 +0600

    contacts chngd

--- send.php
+++ send.php
@@ -1,12 +1,11 @@
 <?
-if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['theme'])&&$_POST['theme']!="")&&(isset($_POST['email'])&&$_POST['email']!="")&&(isset($_POST['message'])&&$_POST['message']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['tel'])&&$_POST['tel']!="")&&(isset($_POST['email'])&&$_POST['email']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
         $to = 'info@fit-office.net'; //Почта получателя, через запятую можно указать сколько угодно адресов
-        $subject = 'Заявка с сайта fit-office'; //Загаловок сообщения
+        $subject = 'Заявка с сайта fit-office'; //Заголовок сообщения
         $from = $_POST['email'];
         $message = ''.$subject.'
                 Имя: '.$_POST['name'].'
-                      Тема: '.$_POST['theme'].'
-                        Сообщение: '.$_POST['message'].'                    
+                      Телефон: '.$_POST['theme'].'                   
                         email: '.$_POST['email'].''; //Текст нащего сообщения можно использовать HTML теги
         $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
         $headers = 'From:' .$from.''; //Наименование и почта отправителя
