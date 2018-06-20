757295819e1c46d46b3d530cdfc32603047be28c
Your Name <you@example.com>
2018-06-20 12:32:19 +0600

    send.php chngd, alert added

--- send.php
+++ send.php
@@ -10,8 +10,20 @@ if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['tel'])&&$_POST['t
         $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
         $headers = 'From:' .$from.''; //Наименование и почта отправителя
         mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
-//        echo $message;
-        header("Location: http://www.fit-office.net");
+                                                         
+        if(mail($to, $subject, $header)){
+            echo "<script>alert('Заявка принята, с вами скоро свяжутся');
+            document.location.href='/index.html';</script>";
+            
+            header("Location: http://www.fit-office.net");
+            
+        }  else {
+            echo "<script>alert('Изивните, сервис временно не работает (не отправлено)');
+            document.location.href='/index.html';</script>";
+        }
+            
+       
+        
 }
 ?>
 
