87b1844c7e429de07c09d6903562c7060d2676b5
Your Name <you@example.com>
2018-06-20 13:31:16 +0600

    send.php chngd, empty fields issue fxd

--- send.php
+++ send.php
@@ -1,5 +1,5 @@
 <?
-if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['tel'])&&$_POST['tel']!="")&&(isset($_POST['email'])&&$_POST['email']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['tel']))&&(isset($_POST['email']))){ //Проверка отправилось ли наше поля name и не пустые ли они
         $to = 'info@fit-office.net'; //Почта получателя, через запятую можно указать сколько угодно адресов
         $subject = 'Заявка с сайта fit-office'; //Заголовок сообщения
         $from = $_POST['email'];
@@ -11,19 +11,20 @@ if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['tel'])&&$_POST['t
         $headers = 'From:' .$from.''; //Наименование и почта отправителя
         mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
                                                          
-        if(mail($to, $subject, $header)){
-            echo "<script>alert('Заявка принята, с вами скоро свяжутся');
+
+        echo "<script>alert('Заявка принята, с вами скоро свяжутся');
             document.location.href='/index.html';</script>";
             
-            header("Location: http://www.fit-office.net");
+        header("Location: http://www.fit-office.net");
             
-        }  else {
-            echo "<script>alert('Изивните, сервис временно не работает (не отправлено)');
-            document.location.href='/index.html';</script>";
-        }
+          
             
        
         
 }
+else {
+    echo "<script>alert('Извините, сервис временно не работает (не отправлено)');
+            document.location.href='/index.html';</script>";
+        }
 ?>
 
