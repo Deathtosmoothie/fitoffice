c4a3aff7b46cdaa5261a76b702eaacc5536a1400
Your Name <you@example.com>
2018-06-26 15:59:24 +0600

    no-email bug fxd

--- send.php
+++ send.php
@@ -2,13 +2,22 @@
 if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['tel']))&&(isset($_POST['email']))){ //Проверка отправилось ли наше поля name и не пустые ли они
         $to = 'info@fit-office.net'; //Почта получателя, через запятую можно указать сколько угодно адресов
         $subject = 'Заявка с сайта fit-office'; //Заголовок сообщения
-        $from = $_POST['email'];
+        
+        if($_POST['email'] == "") {
+            $from = 'no-email';
+        }
+        
+        else {
+            $from = $_POST['email'];
+        }
+    
         $message = ''.$subject.'
                 Имя: '.$_POST['name'].'
                       Телефон: '.$_POST['tel'].'                   
                         email: '.$_POST['email'].''; //Текст нащего сообщения можно использовать HTML теги
         $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
         $headers = 'From:' .$from.''; //Наименование и почта отправителя
+        
         mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
                    
 
